f9d37487499a32a0b0748b79a5943d313c3632c5
PiaSommerauer <pia.sommerauer@live.com>
2020-03-25 16:33:49 +0100

    updating analyses

--- scripts/analyze_pairs.py
+++ scripts/analyze_pairs.py
@@ -3,7 +3,6 @@ from utils_analysis import load_contradiction_pairs
 from utils_analysis import collect_contradictions
 from utils_analysis import sort_by_key
 
-
 from collections import Counter
 import pandas as pd
 import os
@@ -51,6 +50,7 @@ def get_tests_and_checks(worker_dict_list):
                 worker = d['workerid']
                 fails.append(d['description'])
     return fails
+
 
 def get_pair_analysis(data_dict_list, name):
 
@@ -108,6 +108,61 @@ def show_pairs_of_worker(worker, df):
             print(f'{pair} \t total workers contradicting themselves: {len(workers_cont)}')
 
 
+
+def get_ratio_contradicting_pair_annotations(df):
+
+    n_worker_pairs_total = 0.0
+    n_worker_pairs_contradicting = 0.0
+
+    for ind, row in df.iterrows():
+        n_worker_pairs_total += row['n_workers']
+        n_worker_pairs_contradicting += row['n_workers_contradicting']
+
+    if n_worker_pairs_contradicting != 0:
+        ratio = n_worker_pairs_contradicting / n_worker_pairs_total
+    else:
+        ratio = 0.0
+    return ratio
+
+
+
+def comparison_general(name1, name2, df1, df2):
+
+    ratio1 = get_ratio_contradicting_pair_annotations(df1)
+    ratio2 = get_ratio_contradicting_pair_annotations(df2)
+
+    print(f'Set {name1} as a contradiction ratio of {ratio1}')
+    print(f'Set {name2} as a contradiction ratio of {ratio2}')
+    print('The ratio is based on the number of workers annotating a pair.')
+    print('A worker always annotates a full set.')
+    return ratio1, ratio2
+
+def comparison_matching_pairs(name1, name2, df1, df2):
+
+    # get overlapping pairs
+    pairs_df1 = set([row['pair'] for ind, row in df1.iterrows()])
+    pairs_df2 = set([row['pair'] for ind, row in df2.iterrows()])
+    shared_pairs = pairs_df1.intersection(pairs_df2)
+
+    rows_df1_clean = [row for ind, row in df1.iterrows() if row['pair'] in shared_pairs]
+    rows_df2_clean = [row for ind, row in df2.iterrows() if row['pair'] in shared_pairs]
+
+    df1_clean = pd.DataFrame(rows_df1_clean)
+    df2_clean = pd.DataFrame(rows_df2_clean)
+
+    #df_add_row = df_merge_col.append(add_row, ignore_index=True)
+    ratio1, ratio2 = comparison_general(name1, name2, df1_clean, df2_clean)
+    print(f'This analysis only includes pairs annotated in run {name1} and run {name2}.')
+    return ratio1, ratio2
+
+def compare_runs(name1, name2, df1, df2, comp = 'all'):
+    if comp == 'all':
+        r1, r2 = comparison_general(name1, name2, df1, df2)
+    elif comp == 'pairs':
+        r1, r2 = comparison_matching_pairs(name1, name2, df1, df2)
+    return r1, r2
+
+
 def main():
     # analyze all data:
     run = '3'
@@ -119,6 +174,32 @@ def main():
     name = f'run{run}-group_{group}-batch{batch}'.replace('*', '-all-')
     df, filepath = get_pair_analysis(data_dict_list, name)
     print(f'analysis can be found at: {filepath}')
+
+    run = '1'
+    batch = '*'
+    n_q = '*'
+    group = 'experiment1'
+
+    data_dict_list = load_experiment_data(run, group, n_q, batch, remove_not_val = True)
+    name = f'run{run}-group_{group}-batch{batch}'.replace('*', '-all-')
+    df1, filepath = get_pair_analysis(data_dict_list, name)
+    print(f'analysis can be found at: {filepath}')
+
+    run = '3'
+    batch = '*'
+    n_q = '*'
+    group = 'experiment1'
+
+    data_dict_list = load_experiment_data(run, group, n_q, batch, remove_not_val = True)
+    name = f'run{run}-group_{group}-batch{batch}'.replace('*', '-all-')
+    df2, filepath = get_pair_analysis(data_dict_list, name)
+    print(f'analysis can be found at: {filepath}')
+
+    name1 = '1'
+    name2 = '3'
+    compare_runs(name1, name2, df1, df2, comp = 'all')
+    print()
+    compare_runs(name1, name2, df1, df2, comp = 'pairs')
 
 if __name__ == '__main__':
     main()
